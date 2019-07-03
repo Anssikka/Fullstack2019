e172a3ab61869e72ba3c16e6e7fb98f16f8ede06
Anssi Kittilä <Anssikka@gmail.com>
2019-07-03 18:12:18 +0300

    1.4 valmis

--- osa1/1.1-1.2/src/index.js
+++ osa1/1.1-1.2/src/index.js
@@ -38,24 +38,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
     <Header coursename = {course} />
-    <Content palat = {[part1.name, part2.name, part3.name]} tehtavat = {[part1.exercises, part2.exercises, part3.exercises]} />
-    <Total total = {part1.exercises + part2.exercises + part3.exercises }/>
+    <Content palat = {[parts[0].name, parts[1].name, parts[2].name]} tehtavat = {[parts[0].exercises, parts[1].exercises, parts[2].exercises]} />
+    <Total total = {parts[0].exercises + parts[1].exercises + parts[2].exercises }/>
     </div>
     
     
