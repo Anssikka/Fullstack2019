8a168a1746741a68ff89be307df9d86a8556f10c
Anssi Kittilä <Anssikka@gmail.com>
2019-07-05 13:34:33 +0300

    Made a separate component folder for 2.5 and added keys to elements

--- osa2/2.1/src/index.js
+++ osa2/2.1/src/index.js
@@ -1,39 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Course from './Components/Course'
 
-const Header = props =>
-  <h1>{props.course}</h1>
-
-const Total = props => {
-  const total = props.parts.map(a => a.exercises).reduce((a, b) => a + b)
-
-  return <p>yhteensä {total} tehtävää</p>
-}
-  
-const Course = (props) => {
-  return (
-  props.course.map((cours) => {
-    return (
-      <div>
-      <Header course={cours.name} />
-      <Content parts={cours.parts} />
-      <Total parts={cours.parts} />
-    </div>
-    )
-  })
-  )
-}
-
-const Part = ({name, exercises}) =>
-  <p>{name} {exercises}</p>
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.parts.map((v, i) => <Part key={i} name={v.name} exercises={v.exercises} />)}
-    </div>
-  )
-} 
 
 const App = () => {
   const courses = [
