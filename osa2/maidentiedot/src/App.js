e6d4c5f1a28aa88dfb8134b8cba86a5ff433ff35
Anssi Kittilä <Anssikka@gmail.com>
2019-07-10 12:29:53 +0300

    2.12 done

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+
 const Finder = (props) => {
   return(
   <div>
@@ -11,9 +12,64 @@ const Finder = (props) => {
 }
 
 
+const ShowCountries = (props) => {
+  const filteredCountries = props.countries.filter((o) => o.name.toUpperCase().includes(props.filterW.toUpperCase()));
+  
+  if (filteredCountries.length === 1) {
+    return (
+      <div>
+        <SingleCountry country = {filteredCountries[0]} />
+      </div>
+    )
+  } else if (filteredCountries.length > 1 && filteredCountries.length < 10) {
+    return (
+      <div>
+        <Country countryA={filteredCountries} />
+      </div>
+    )
+  } else if (filteredCountries.length === 0) {
+    return <p>No matches sorry!</p>
+  } 
+
+    return (
+      <p>Too many matches, try filtering!</p>
+    )
+  
+}
+
+
+const Country = (props) => {
+  if (props.countryA.length > 1) {
+  return (
+    props.countryA.map((o, i) => {
+      return (<div key={i}><p>{o.name}</p></div>)
+    })   
+  )}
+
+}
+
+
+  const SingleCountry = (props) => {
+    const o = props.country
+    const languages = props.country.languages
+ 
+    return (
+      <div key={o.name}>
+        <h2>{o.name}</h2>
+        <p>Capital: {o.capital}</p>
+        <p>Population: {o.population}</p>
+        <h3>Languages</h3>
+        {languages.map(la => <li key={la.name}>{la.name}</li>)}
+        <img src={o.flag} alt="flag" width="200" height="150"/>
+
+      </div>
+    )
+  }
+
+
 function App() {
   const [ filterBox, setFilterBox] = useState('')
-  const [ countries, setCountries] = useState([{ name:'', capital:'', population:''}])
+  const [ countries, setCountries] = useState([{ name:'', capital:'', population:''},{ name:'', capital:'', population:''}])
 
   const handleFilter = (event) => {
     setFilterBox(event.target.value)
@@ -25,7 +81,10 @@ function App() {
       const datat = response.data.map(o => {
         return (
           {name: o.name,
-          population: o.population}
+          population: o.population,
+          capital: o.capital,
+          languages: o.languages,
+          flag: o.flag}
         )
         
       })
@@ -33,17 +92,16 @@ function App() {
     })
   }
 
-  
-  
   useEffect(hook,[])
-  console.log(countries)
 
   return (
     <div>
       <h1>Country finder:</h1>
     <Finder filterBox={filterBox} handleFilter={handleFilter} />
+    <ShowCountries countries={countries} filterW={filterBox} />
     </div>
   );
 }
+
 
 export default App;
